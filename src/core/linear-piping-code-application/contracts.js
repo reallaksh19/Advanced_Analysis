8b75ac342e9803d275d7e85d460f17dddfe3978b
reallaksh19 <reallaksh19@gmail.com>
2026-07-31 08:15:35 +0400

    refactor: distinguish B31 application request and result schemas

--- src/core/linear-piping-code-application/contracts.js
+++ src/core/linear-piping-code-application/contracts.js
@@ -6,6 +6,7 @@ import { semanticHash } from '../shared-piping-model/canonical-json.js';
 
 export const NOZZLE_ALLOWABLE_PROFILE_SCHEMA = 'linear-piping-nozzle-allowable-profile/v1';
 export const NOZZLE_ASSESSMENT_SCHEMA = 'linear-piping-nozzle-assessment/v1';
+export const B31_APPLICATION_REQUEST_SCHEMA = 'linear-piping-b31-application-request/v1';
 export const B31_APPLICATION_SCHEMA = 'linear-piping-b31-application/v1';
 export const APPLICATION_RESULT_SCHEMA = 'linear-piping-qualified-application-result/v1';
 
