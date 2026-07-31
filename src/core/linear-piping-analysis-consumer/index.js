051573c878d84652d135ecefd53b5f45ba9b0eab
reallaksh19 <reallaksh19@gmail.com>
2026-07-31 14:24:24 +0400

    feat(lfea): export Phase 2F unit normalization contracts

--- src/core/linear-piping-analysis-consumer/index.js
+++ src/core/linear-piping-analysis-consumer/index.js
@@ -43,8 +43,10 @@ export {
   INPUTXML_CONDITIONING_KEYS,
   INPUTXML_INGESTION_EVIDENCE_KEYS,
   INPUTXML_INGESTION_KEYS,
+  INPUTXML_INGESTION_V2_KEYS,
   INPUTXML_SOURCE_KEYS,
   LINEAR_PIPING_INPUTXML_ANALYSIS_REQUEST_SCHEMA,
+  LINEAR_PIPING_INPUTXML_ANALYSIS_REQUEST_V2_SCHEMA,
   LINEAR_PIPING_INPUTXML_ANALYSIS_RESULT_SCHEMA,
   LINEAR_PIPING_INPUTXML_SOURCE_SCHEMA,
   computeInputXmlAnalysisResultEvidenceHash,
@@ -55,6 +57,25 @@ export {
   requireLinearPipingInputXmlSource,
   sealLinearPipingInputXmlSource,
 } from './inputxml-source-contract.js';
+
+export {
+  INPUTXML_LENGTH_UNIT_REGISTRY_ID,
+  INPUTXML_UNIT_PROFILE_KEYS,
+  INPUTXML_UNIT_RESULT_KEYS,
+  LINEAR_PIPING_INPUTXML_UNIT_PROFILE_SCHEMA,
+  LINEAR_PIPING_INPUTXML_UNIT_RESULT_SCHEMA,
+  computeInputXmlUnitProfileSemanticHash,
+  computeInputXmlUnitResultEvidenceHash,
+  computeInputXmlUnitResultSemanticHash,
+  inputXmlGeometryProjection,
+  inputXmlLengthUnitDefinition,
+  inputXmlUnitEvidenceProjection,
+  requireLinearPipingInputXmlUnitProfile,
+  requireLinearPipingInputXmlUnitResult,
+  sealLinearPipingInputXmlUnitProfile,
+} from './inputxml-unit-contract.js';
+
+export { normalizeLinearPipingInputXmlGeometry } from './inputxml-unit-normalization.js';
 
 export {
   INPUTXML_ANALYSIS_CONTEXT_KEYS,
