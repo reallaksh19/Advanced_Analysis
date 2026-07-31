5b4e0971b776cca3cb0a30ac4a73a2923f18df11
reallaksh19 <reallaksh19@gmail.com>
2026-07-31 22:12:01 +0400

    feat(lafea): add T6C live registration public surface

---
+++ src/workspace/lafea-templates/t6c-live-registration.js
@@ -0,0 +1,7 @@
+export {
+  LAFEA_TEMPLATE_WORKBENCH_REGISTRATION_AUTHORITY,
+  LAFEA_TEMPLATE_WORKBENCH_REGISTRATION_SCHEMA,
+  LAFEA_TEMPLATE_WORKBENCH_REGISTRATION_STATUS,
+  createLafeaTemplateWorkbenchRegistration,
+  mountLafeaTemplateWorkbench,
+} from './workbench-registration.js';
