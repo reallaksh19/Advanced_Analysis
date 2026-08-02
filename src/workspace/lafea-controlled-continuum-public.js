8f8f345facc619385b921d2d73f52e773ffef6a1
reallaksh19 <reallaksh19@gmail.com>
2026-08-02 09:45:16 +0400

    feat(lafea): export NB-T6C batch surface

--- src/workspace/lafea-controlled-continuum-public.js
+++ src/workspace/lafea-controlled-continuum-public.js
@@ -3,7 +3,7 @@ export {
   LAFEA_CONTROLLED_CONTINUUM_CONTROLLER_RESULT_SCHEMA,
   LAFEA_CONTROLLED_CONTINUUM_CONTROLLER_REVISION,
   executeControlledLafeaContinuumPilot,
-} from './lafea-controlled-continuum-controller.js';
+} from './lafea-controlled-continuum-execution-public.js';
 export {
   LAFEA_CONTROLLED_CONTINUUM_STAGE_ID,
   LAFEA_CONTROLLED_CONTINUUM_STAGE_ROUTE_SCHEMA,
@@ -17,3 +17,15 @@ export {
   lafeaLugPinholeAnalysisGeometryHash,
   validateLafeaLugPinholeMeshLadder,
 } from './lafea-lug-pinhole-mesh-ladder.js';
+export {
+  LAFEA_LUG_PINHOLE_EXECUTION_INTAKE_SCHEMA,
+  LAFEA_LUG_PINHOLE_EXECUTION_SCHEMA,
+  LAFEA_LUG_PINHOLE_FEATURE_PROJECTION_SCHEMA,
+  LAFEA_LUG_PINHOLE_PHYSICAL_PROBLEM_PRODUCER_REVISION,
+  LAFEA_LUG_PINHOLE_PHYSICAL_PROBLEM_SCHEMA,
+  LAFEA_LUG_PINHOLE_PROJECTION_INTAKE_SCHEMA,
+  LAFEA_LUG_PINHOLE_PROJECTION_SCHEMA,
+  createLafeaLugPinholePhysicalProblemProjection,
+  executeLafeaLugPinholePhysicalProblemBatch,
+  validateLafeaLugPinholePhysicalProblemProjection,
+} from './lafea-lug-pinhole-physical-problem-batch.js';
