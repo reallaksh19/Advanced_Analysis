5ca3c9de630e5cd3d027218af470b2322e9224a0
reallaksh19 <reallaksh19@gmail.com>
2026-08-08 10:35:21 +0400

    Issue #907: add reachable typed viewport backend seam

---
+++ src/workspace/topology-edit/topology-edit-reachable-typed-viewport-backend.js
@@ -0,0 +1,3 @@
+import { TopologyEditTypedViewportBackend } from './topology-edit-typed-viewport-backend.js';
+
+export class TopologyEditReachableTypedViewportBackend extends TopologyEditTypedViewportBackend {}
