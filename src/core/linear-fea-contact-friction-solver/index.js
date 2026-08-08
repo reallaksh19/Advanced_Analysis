e54a4acb67f1670fc619e83b4873fb82e199971d
reallaksh19 <reallaksh19@gmail.com>
2026-08-08 08:47:05 +0400

    M041: export contact-friction solver API

---
+++ src/core/linear-fea-contact-friction-solver/index.js
@@ -0,0 +1,17 @@
+export {
+  CONTACT_FRICTION_CLASSIFICATION,
+  CONTACT_FRICTION_EXECUTION_SCHEMA,
+  CONTACT_FRICTION_POLICY_SCHEMA,
+  CONTACT_FRICTION_STATE,
+  DEFAULT_CONTACT_FRICTION_POLICY,
+  computeContactFrictionExecutionSemanticHash,
+  contactFrictionExecutionSemanticProjection,
+  normalizeContactFrictionDeclarations,
+  requireContactFrictionDeclaration,
+  resolveContactFrictionPolicy,
+  sealContactFrictionExecution,
+} from './contact-friction-contract.js';
+
+export { solveFixedContactState, stateSnapshot } from './contact-response.js';
+export { evaluateContactFrictionState } from './state-evaluation.js';
+export { compileContactFrictionExecution, contactFrictionStateFromRows } from './iteration.js';
